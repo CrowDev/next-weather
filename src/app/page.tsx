6f0ca6910d6de29e2b84c41f347b85738dce5ab8
CrowDev <h.aguilarvalenzuela@gmail.com>
2023-11-24 04:00:30 -0300

    fetching current conditions from hardcoded location

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,12 @@ import { MainWeather } from './ui/main-weather'
 import { WeatherByTime } from './ui/weather-by-time'
 import { CurrentConditions } from './ui/current-conditions'
 import { Forecast } from './ui/forecast'
+import { CurrentConditions as TCurrentConditions } from './@types'
+
+const BASE_URL = process.env.ACCU_API
+const KEY = process.env.ACCU_KEY
 
 const cityFetching = async (cityName = '') => {
-  const BASE_URL = process.env.ACCU_API
-  const KEY = process.env.ACCU_KEY
   const ENDPOINT = `${BASE_URL}/locations/v1/cities/search?apikey=${KEY}&q=${cityName}`
   const res = await fetch(ENDPOINT)
   if (!res.ok) {
@@ -18,13 +20,20 @@ const cityFetching = async (cityName = '') => {
   return cityKey
 }
 
+const currentConditionFetching = async (cityKey: string) => {
+  const ENDPOINT = `${BASE_URL}/currentconditions/v1/${cityKey}?apikey=${KEY}`
+  const res = await fetch(ENDPOINT)
+  return res.json()
+}
+
 export default async function Dashboard() {
   const cityKey = await cityFetching('New York')
+  const [currentConditions]: TCurrentConditions[] = await currentConditionFetching(cityKey)
   return (
     <div>
       <Suspense fallback={<Loading />}>
-        <main className='grid grid-rows-[200px_150px_200px] grid-cols-[70%_30%] grid-flow-col gap-5'>
-          <MainWeather />
+        <main className='grid grid-rows-[200px_150px_200px] grid-cols-[30%_70%] grid-flow-col gap-5'>
+          <MainWeather currentConditions={currentConditions} />
           <WeatherByTime />
           <CurrentConditions />
           <Forecast />
