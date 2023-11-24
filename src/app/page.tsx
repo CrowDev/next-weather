56bb586311a7a240578ea4707130eef772826b52
CrowDev <h.aguilarvalenzuela@gmail.com>
2023-11-23 23:28:53 -0300

    added first city search hardcoded

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
-export default function Dashboard() {
+import { Suspense } from 'react'
+import Loading from './loading'
+
+const cityFetching = async (cityName = '') => {
+  const BASE_URL = process.env.ACCU_API
+  const KEY = process.env.ACCU_KEY
+  const ENDPOINT = `${BASE_URL}/locations/v1/cities/search?apikey=${KEY}&q=${cityName}`
+  const res = await fetch(ENDPOINT)
+  if (!res.ok) {
+    throw new Error('Failed to fetch')
+  }
+  const data = await res.json()
+  const cityKey = data.find((data: any) => data.Country.ID === 'US').Key
+  return cityKey
+}
+
+export default async function Dashboard() {
+  const cityKey = await cityFetching('New York')
   return (
     <section>
-      <h1>Dashboard</h1>
+      <Suspense fallback={<Loading />}>
+        {cityKey}
+      </Suspense>
     </section>
   )
 }
