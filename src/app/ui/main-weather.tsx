e76dda399ca893237bbf519ac5f371c847ec77bb
CrowDev <h.aguilarvalenzuela@gmail.com>
2023-11-27 22:55:23 -0300

    fixed missing icon for main weather

--- src/app/ui/main-weather.tsx
+++ src/app/ui/main-weather.tsx
@@ -6,7 +6,8 @@ interface Props {
 }
 
 export const MainWeather = ({ currentConditions }: Props) => {
-	const src = `https://developer.accuweather.com/sites/default/files/${currentConditions.WeatherIcon}-s.png`
+	const icon = currentConditions.WeatherIcon < 10 ? `0${currentConditions.WeatherIcon}` : currentConditions.WeatherIcon
+	const src = `https://developer.accuweather.com/sites/default/files/${icon}-s.png`
 	return (
 		<section className='bg-slate-800 rounded-lg p-5 grid grid-rows-3 grid-cols-[2fr_100px] grid-flow-col'>
 			<h2 className='text-3xl'>New York</h2>
