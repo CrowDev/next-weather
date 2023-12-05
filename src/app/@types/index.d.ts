05d28aac42529d36be2f97d148c2a2d19295d181
CrowDev <h.aguilarvalenzuela@gmail.com>
2023-12-04 23:50:48 -0300

    added hourly types

--- src/app/@types/index.d.ts
+++ src/app/@types/index.d.ts
@@ -96,3 +96,80 @@ export type Visibility = {
 	Metric:   Imum;
 	Imperial: Imum;
 }
+
+export type Hourly = {
+	DateTime:                 Date;
+	EpochDateTime:            number;
+	WeatherIcon:              number;
+	IconPhrase:               string;
+	HasPrecipitation:         boolean;
+	IsDaylight:               boolean;
+	Temperature:              Ceiling;
+	RealFeelTemperature:      Ceiling;
+	RealFeelTemperatureShade: Ceiling;
+	WetBulbTemperature:       Ceiling;
+	DewPoint:                 Ceiling;
+	Wind:                     Wind;
+	WindGust:                 WindGust;
+	RelativeHumidity:         number;
+	IndoorRelativeHumidity:   number;
+	Visibility:               Ceiling;
+	Ceiling:                  Ceiling;
+	UVIndex:                  number;
+	UVIndexText:              UVIndexText;
+	PrecipitationProbability: number;
+	ThunderstormProbability:  number;
+	RainProbability:          number;
+	SnowProbability:          number;
+	IceProbability:           number;
+	TotalLiquid:              Ceiling;
+	Rain:                     Ceiling;
+	Snow:                     Ceiling;
+	Ice:                      Ceiling;
+	CloudCover:               number;
+	Evapotranspiration:       Ceiling;
+	SolarIrradiance:          Ceiling;
+	MobileLink:               string;
+	Link:                     string;
+}
+
+export type Ceiling = {
+	Value:    number;
+	Unit:     Unit;
+	UnitType: number;
+	Phrase?:  Phrase;
+}
+
+export enum Phrase {
+	Chilly = "Chilly",
+	Cold = "Cold",
+}
+
+export enum Unit {
+	C = "C",
+	CM = "cm",
+	KM = "km",
+	KMH = "km/h",
+	M = "m",
+	Mm = "mm",
+	WM = "W/m²",
+}
+
+export enum UVIndexText {
+	Low = "Low",
+}
+
+export type Wind = {
+	Speed:     Ceiling;
+	Direction: Direction;
+}
+
+export type Direction = {
+	Degrees:   number;
+	Localized: string;
+	English:   string;
+}
+
+export type WindGust = {
+	Speed: Ceiling;
+}
