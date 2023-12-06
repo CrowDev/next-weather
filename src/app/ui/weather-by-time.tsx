1cbe8f3fa0a7ccf1439967dce9496eab62683477
CrowDev <h.aguilarvalenzuela@gmail.com>
2023-12-06 18:40:13 -0300

    get hour and styled grid for hours

--- src/app/ui/weather-by-time.tsx
+++ src/app/ui/weather-by-time.tsx
@@ -7,25 +7,20 @@ interface Props {
 
 export const WeatherByTime = ({hourlyForecast}: Props) => {
 	const getDateInfo = (item: Hourly) => {
-		const date = DateTime.fromMillis(item.EpochDateTime * 1000)
-		const day = date.day
-		const month = date.month
-		const weekDay = date.weekdayShort
-		return {
-			day,
-			month,
-			weekDay
-		}
+		const hour = DateTime.fromMillis(item.EpochDateTime * 1000).toLocaleString(DateTime.TIME_24_SIMPLE)
+		return hour
 	}
 	return (
-		<section className='bg-slate-800 rounded-lg p-5 grid grid-cols-6 place-items-center'>
-			{hourlyForecast.map((hour: Hourly) => (
-				<article key={hour.EpochDateTime} className='flex flex-col items-center'>
-					<p className='text-white text-sm'>{getDateInfo(hour).day}</p>
-					{/* <img src={`https://developer.accuweather.com/sites/default/files/${hour.WeatherIcon < 10 ? `0${hour.WeatherIcon}` : hour.WeatherIcon}-s.png`} alt={hour.IconPhrase} /> */}
-					<p className='text-white text-sm'>{hour.Temperature.Value}°{hour.Temperature.Unit}</p>
-				</article>
-			))}
+		<section className='bg-slate-800 rounded-lg p-5 flex flex-col gap-3'>
+			<p className='text-white font-bold'>Weather by Time</p>
+			<article className='grid grid-cols-6 grow'>
+				{hourlyForecast.map((hour: Hourly) => (
+					<article key={hour.EpochDateTime} className='flex flex-col'>
+						<p className='text-white text-sm'>{getDateInfo(hour)}</p>
+						<p className='text-white font-medium'>{hour.Temperature.Value}°{hour.Temperature.Unit}</p>
+					</article>
+				))}
+			</article>
 		</section>
 	)	
 }
